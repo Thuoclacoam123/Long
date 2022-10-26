c021080a8591d38ce68c2d0300a9f34540254274
Thuoclacoam123 <116324392+Thuoclacoam123@users.noreply.github.com>
2022-10-26 08:27:21 +0700

    Create buoi2.cpp

---
+++ buoi2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	int a ;
+	int b ;
+	scanf("%d",&a);
+	scanf("&d",&b);
+	
+    if(a>b){
+    	printf("max la %&",a);
+	}else{
+		if(a==b){
+			printf("Hai so bang nhau");
+		}else{
+			printf("%& la max",b);
+		}
+	}
+	
+
+
+	
+}
